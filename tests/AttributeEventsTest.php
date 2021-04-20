055ab82aa529382efa19bdaaa65db858f0544e5e
Bhushan <bhushangaykawad@gmail.com>
2021-04-20 17:03:57 +0530

    added feedback changes

--- tests/AttributeEventsTest.php
+++ tests/AttributeEventsTest.php
@@ -1,28 +1,11 @@
 <?php
 
-namespace Kleemans\Tests\Unit;
+namespace Kleemans\Tests;
 
 use Illuminate\Database\Capsule\Manager as DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Support\Testing\Fakes\EventFake;
-use Kleemans\Tests\Fake\Events\InvoiceDownloaded;
-use Kleemans\Tests\Fake\Events\OrderCanceled;
-use Kleemans\Tests\Fake\Events\OrderDeleted;
-use Kleemans\Tests\Fake\Events\OrderMadeFree;
-use Kleemans\Tests\Fake\Events\OrderMetaUpdated;
-use Kleemans\Tests\Fake\Events\OrderNoteUpdated;
-use Kleemans\Tests\Fake\Events\OrderPaid;
-use Kleemans\Tests\Fake\Events\OrderPaidHandlingFee;
-use Kleemans\Tests\Fake\Events\OrderPaidWithCash;
-use Kleemans\Tests\Fake\Events\OrderPlaced;
-use Kleemans\Tests\Fake\Events\OrderReturned;
-use Kleemans\Tests\Fake\Events\OrderShipped;
-use Kleemans\Tests\Fake\Events\OrderShippingCountryChanged;
-use Kleemans\Tests\Fake\Events\OrderTaxCleared;
-use Kleemans\Tests\Fake\Events\OrderUpdated;
-use Kleemans\Tests\Fake\Events\PaypalPaymentDenied;
-use Kleemans\Tests\Fake\Order;
 use PHPUnit\Framework\TestCase;
 
 class AttributeEventsTest extends TestCase
@@ -30,22 +13,22 @@ class AttributeEventsTest extends TestCase
     private $dispatcher;
 
     private static $eventsToFake = [
-        OrderPlaced::class,
-        OrderUpdated::class,
-        OrderDeleted::class,
-        OrderNoteUpdated::class,
-        OrderShipped::class,
-        OrderCanceled::class,
-        OrderReturned::class,
-        OrderMadeFree::class,
-        OrderPaidHandlingFee::class,
-        OrderTaxCleared::class,
-        OrderShippingCountryChanged::class,
-        OrderPaid::class,
-        OrderPaidWithCash::class,
-        OrderMetaUpdated::class,
-        PaypalPaymentDenied::class,
-        InvoiceDownloaded::class,
+        Fake\Events\OrderPlaced::class,
+        Fake\Events\OrderUpdated::class,
+        Fake\Events\OrderDeleted::class,
+        Fake\Events\OrderNoteUpdated::class,
+        Fake\Events\OrderShipped::class,
+        Fake\Events\OrderCanceled::class,
+        Fake\Events\OrderReturned::class,
+        Fake\Events\OrderMadeFree::class,
+        Fake\Events\OrderPaidHandlingFee::class,
+        Fake\Events\OrderTaxCleared::class,
+        Fake\Events\OrderShippingCountryChanged::class,
+        Fake\Events\OrderPaid::class,
+        Fake\Events\OrderPaidWithCash::class,
+        Fake\Events\OrderMetaUpdated::class,
+        Fake\Events\PaypalPaymentDenied::class,
+        Fake\Events\InvoiceDownloaded::class,
     ];
 
     public function setUp(): void
@@ -58,49 +41,49 @@ class AttributeEventsTest extends TestCase
     public function it_still_dispatches_native_events(): void
     {
         // Create
-        $order = new Order();
+        $order = new Fake\Order();
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderPlaced::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderPlaced::class);
 
         // Update
         $order->status = 'shipped';
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderUpdated::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderUpdated::class);
 
         // Delete
         $order->delete();
 
-        $this->dispatcher->assertDispatched(OrderDeleted::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderDeleted::class);
     }
 
     /** @test */
     public function it_dispatches_event_on_change()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->save();
 
         $order->note = 'Please deliver before the weekend';
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderNoteUpdated::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderNoteUpdated::class);
     }
 
     /** @test */
     public function it_dispatches_event_on_change_after_find()
     {
-        $order = Order::find(1);
+        $order = Fake\Order::find(1);
         $order->note = 'Please deliver to neighbour';
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderNoteUpdated::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderNoteUpdated::class);
     }
 
     /** @test */
     public function it_dispatches_multiple_events()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->save();
 
         $order->note = 'Please deliver before the weekend';
@@ -108,76 +91,76 @@ class AttributeEventsTest extends TestCase
         $order->tax_free = true;
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderShipped::class);
-        $this->dispatcher->assertDispatched(OrderNoteUpdated::class);
-        $this->dispatcher->assertDispatched(OrderTaxCleared::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderShipped::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderNoteUpdated::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderTaxCleared::class);
     }
 
     /** @test */
     public function it_works_with_update_method()
     {
-        $order = Order::find(1);
+        $order = Fake\Order::find(1);
         $order->update([
             'note' => 'Handle with care',
             'status' => 'canceled',
         ]);
 
-        $this->dispatcher->assertDispatched(OrderNoteUpdated::class);
-        $this->dispatcher->assertDispatched(OrderCanceled::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderNoteUpdated::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderCanceled::class);
     }
 
     /** @test */
     public function it_does_not_dispatch_on_initial_value_of_attribute()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->note = 'Please handle with care';
         $order->save();
 
-        $this->dispatcher->assertNotDispatched(OrderNoteUpdated::class);
+        $this->dispatcher->assertNotDispatched(Fake\Events\OrderNoteUpdated::class);
     }
 
     /** @test */
     public function it_does_not_dispatch_when_same_value()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->note = 'Please handle with care';
         $order->save();
 
         $order->note = 'Please handle with care';
         $order->save();
 
-        $this->dispatcher->assertNotDispatched(OrderNoteUpdated::class);
+        $this->dispatcher->assertNotDispatched(Fake\Events\OrderNoteUpdated::class);
     }
 
     /** @test */
     public function it_does_not_dispatch_on_change_of_other_attribute()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->note = 'Please handle with care';
         $order->save();
 
         $order->status = 'shipped';
         $order->save();
 
-        $this->dispatcher->assertNotDispatched(OrderNoteUpdated::class);
+        $this->dispatcher->assertNotDispatched(Fake\Events\OrderNoteUpdated::class);
     }
 
     /** @test */
     public function it_does_not_dispatch_on_initial_save_of_replication()
     {
-        $order = Order::find(1);
+        $order = Fake\Order::find(1);
 
         $orderCopy = $order->replicate();
         $orderCopy->status = 'canceled';
         $orderCopy->save();
 
-        $this->dispatcher->assertNotDispatched(OrderCanceled::class);
+        $this->dispatcher->assertNotDispatched(Fake\Events\OrderCanceled::class);
     }
 
     /** @test */
     public function it_dispatches_correct_number_of_times()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->note = 'Initial value';
         $order->save();
 
@@ -190,68 +173,68 @@ class AttributeEventsTest extends TestCase
         $order->note = 'Change 2';
         $order->save();
 
-        $this->dispatcher->assertDispatchedTimes(OrderNoteUpdated::class, 2);
-        $this->dispatcher->assertDispatchedTimes(OrderUpdated::class, 2);
-        $this->dispatcher->assertDispatchedTimes(OrderPlaced::class, 1);
+        $this->dispatcher->assertDispatchedTimes(Fake\Events\OrderNoteUpdated::class, 2);
+        $this->dispatcher->assertDispatchedTimes(Fake\Events\OrderUpdated::class, 2);
+        $this->dispatcher->assertDispatchedTimes(Fake\Events\OrderPlaced::class, 1);
     }
 
     /** @test */
     public function it_dispatches_event_on_change_to_specific_string_value()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->save();
 
         $order->status = 'shipped';
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderShipped::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderShipped::class);
     }
 
     /** @test */
     public function it_dispatches_event_on_change_to_specific_int_value()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->save();
 
         $order->discount_percentage = 100;
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderMadeFree::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderMadeFree::class);
     }
 
     /** @test */
     public function it_dispatches_event_on_change_to_specific_float_value()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->save();
 
         $order->paid_amount = 2.99;
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderPaidHandlingFee::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderPaidHandlingFee::class);
     }
 
     /** @test */
     public function it_dispatches_event_on_change_to_specific_boolean_value()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->save();
 
         $order->tax_free = true;
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderTaxCleared::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderTaxCleared::class);
     }
 
     /** @test */
     public function it_respects_withoutEvents()
     {
-        Order::withoutEvents(function () {
-            $order = Order::find(1);
+        Fake\Order::withoutEvents(function () {
+            $order = Fake\Order::find(1);
             $order->status = 'returned';
             $order->save();
 
-            $this->dispatcher->assertNotDispatched(OrderReturned::class);
+            $this->dispatcher->assertNotDispatched(Fake\Events\OrderReturned::class);
         });
     }
 
@@ -260,48 +243,48 @@ class AttributeEventsTest extends TestCase
     /** @test */
     public function it_dispatches_event_on_accessor_change()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->shipping_address = '4073 Hamill Avenue US';
         $order->save();
 
-        $order = new Order();
+        $order = new Fake\Order();
         $order->shipping_address = '9819 Second Ave. US';
         $order->save();
 
-        $this->dispatcher->assertNotDispatched(OrderShippingCountryChanged::class);
+        $this->dispatcher->assertNotDispatched(Fake\Events\OrderShippingCountryChanged::class);
 
         $order->shipping_address = 'Kerkstraat 7 NL';
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderShippingCountryChanged::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderShippingCountryChanged::class);
     }
 
     /** @test */
     public function it_dispatches_event_on_accessor_change_to_specific_value()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->total = 10;
         $order->save();
 
         $order->paid_amount = 5;
         $order->save();
 
-        $this->dispatcher->assertNotDispatched(OrderPaid::class);
+        $this->dispatcher->assertNotDispatched(Fake\Events\OrderPaid::class);
 
         $order->paid_amount = 10;
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderPaid::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderPaid::class);
     }
 
     /** @test */
     public function it_dispatches_event_on_change_of_accessor_for_existing_attribute()
     {
-        $order = Order::find(1);
+        $order = Fake\Order::find(1);
         $order->payment_gateway = 'direct';
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderPaidWithCash::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderPaidWithCash::class);
     }
 
     // JSON attributes
@@ -309,20 +292,20 @@ class AttributeEventsTest extends TestCase
     /** @test */
     public function it_dispatches_event_when_updating_json_attribute()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->meta = ['gift_wrapping' => true];
         $order->save();
 
         $order->meta = ['gift_wrapping' => false];
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderMetaUpdated::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderMetaUpdated::class);
     }
 
     /** @test */
     public function it_dispatches_event_when_updating_json_field()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->meta = ['paypal_status' => 'pending'];
         $order->save();
 
@@ -332,25 +315,25 @@ class AttributeEventsTest extends TestCase
 
         $order->save();
 
-        $this->dispatcher->assertDispatched(PaypalPaymentDenied::class);
+        $this->dispatcher->assertDispatched(Fake\Events\PaypalPaymentDenied::class);
     }
 
     /** @test */
     public function it_works_with_json_changes_through_update_method()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->meta = ['paypal_status' => 'pending'];
         $order->save();
 
         $order->update(['meta->paypal_status' => 'denied']);
 
-        $this->dispatcher->assertDispatched(PaypalPaymentDenied::class);
+        $this->dispatcher->assertDispatched(Fake\Events\PaypalPaymentDenied::class);
     }
 
     /** @test */
     public function it_dispatches_event_when_adding_json_field()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->meta = ['gift_wrapping' => true];
         $order->save();
 
@@ -360,27 +343,27 @@ class AttributeEventsTest extends TestCase
 
         $order->save();
 
-        $this->dispatcher->assertDispatched(PaypalPaymentDenied::class);
+        $this->dispatcher->assertDispatched(Fake\Events\PaypalPaymentDenied::class);
     }
 
     /** @test */
     public function it_does_not_dispatch_on_initial_value_of_json_attribute()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->meta = [
             'gift_wrapping' => true,
             'paypal_status' => 'denied',
         ];
         $order->save();
 
-        $this->dispatcher->assertNotDispatched(OrderMetaUpdated::class);
-        $this->dispatcher->assertNotDispatched(PaypalPaymentDenied::class);
+        $this->dispatcher->assertNotDispatched(Fake\Events\OrderMetaUpdated::class);
+        $this->dispatcher->assertNotDispatched(Fake\Events\PaypalPaymentDenied::class);
     }
 
     /** @test */
     public function it_works_with_nested_json_fields()
     {
-        $order = new Order();
+        $order = new Fake\Order();
         $order->meta = ['invoice' => ['downloaded' => false]];
         $order->save();
 
@@ -390,8 +373,8 @@ class AttributeEventsTest extends TestCase
 
         $order->save();
 
-        $this->dispatcher->assertDispatched(OrderMetaUpdated::class);
-        $this->dispatcher->assertDispatched(InvoiceDownloaded::class);
+        $this->dispatcher->assertDispatched(Fake\Events\OrderMetaUpdated::class);
+        $this->dispatcher->assertDispatched(Fake\Events\InvoiceDownloaded::class);
     }
 
     // Setup methods
