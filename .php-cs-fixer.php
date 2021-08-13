da5a7f06926c4684c6cd2eeb464820efd08ea30b
Jan-Paul Kleemans <jpkleemans@gmail.com>
2021-08-13 15:19:16 +0200

    Update PHP CS Fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,12 +1,12 @@
 <?php
 
 return (new PhpCsFixer\Config())
+    ->setFinder(PhpCsFixer\Finder::create()
+        ->exclude('vendor')
+        ->in(__DIR__))
+    ->setUsingCache(false)
     ->setRules([
         '@Symfony' => true,
         'yoda_style' => false,
         'php_unit_method_casing' => false,
-    ])
-    ->setFinder(PhpCsFixer\Finder::create()
-        ->exclude('vendor')
-        ->in(__DIR__))
-    ->setUsingCache(false);
+    ]);
